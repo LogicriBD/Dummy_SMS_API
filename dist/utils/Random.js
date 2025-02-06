894ab247e132dd11611a6e3992692969bb680912
mirzaazwad <mirzaazwad8@gmail.com>
2025-02-06 17:07:52 +0600

    Render

--- dist/utils/Random.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.randomUsername = void 0;
-const crypto_1 = require("crypto");
-const randomUsername = () => (0, crypto_1.randomUUID)().split('-').pop();
-exports.randomUsername = randomUsername;
-//# sourceMappingURL=Random.js.map
